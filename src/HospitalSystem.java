84dc3a3ea1f879cdadd9961451532b310387f208
tugba <tugbaalkoc0@gmail.com>
2025-12-22 16:42:06 +0300

    Initial commit

--- src/HospitalSystem.java
+++ src/HospitalSystem.java
@@ -2,20 +2,59 @@ import java.util.HashMap;
 
 public class HospitalSystem {
     PatientList patientList;
-    TreatmentQueue treatmentQueue;
+    PriorityTreatmentQueue treatmentQueue;
     DischargeStack dischargeStack;
     HashMap<Integer, Patient> patientHashMap;
     public HospitalSystem(){
         patientList = new PatientList();
-        treatmentQueue = new TreatmentQueue();
+        treatmentQueue = new PriorityTreatmentQueue();
         dischargeStack = new DischargeStack();
         patientHashMap = new HashMap<>();
     }
-
-    public void addPatient(Patient patient){
+    //admitted patients
+    void addPatient(Patient patient){
         patientList.addPatient(patient);
         patientHashMap.put(patient.ID, patient);
     }
+    // change queue for treatment by priority
+    void addTreatmentRequest(int patientId,boolean priority){
+        if(!patientHashMap.containsKey(patientId)){
+            System.out.println("Patient not found");
+            return;
+        }
+        TreatmentRequest request = new TreatmentRequest(patientId,System.currentTimeMillis(),priority);
+        treatmentQueue.enqueue(request);
+    }
+    // Treated patient and discharge them
+    void processTreatment(){
+        TreatmentRequest request = treatmentQueue.dequeue();
+        if(request != null){
+            DischargeRecord record = new DischargeRecord(request.patientId,System.currentTimeMillis());
+            dischargeStack.push(record);
+        }
+
+    }
+    // record the discharges
+    void addDischargeRecord(DischargeRecord record){
+        dischargeStack.push(record);
+    }
+    void printSystem(){
+        System.out.println("PATIENTS:");
+        patientList.printList();
+
+        System.out.println("-----------");
+
+        System.out.println("TREATMENT QUEUE");
+        treatmentQueue.printQueue();
+
+        System.out.println("-----------");
+
+        System.out.println("DISCHARGE STACK");
+        dischargeStack.printStack();
+
+
+    }
+
 
 
 }
